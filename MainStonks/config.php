fc40d9b2ebb0b19c4b3ef1738e344108093a6525
Axebita <Axel.baudoin@epsi.fr>
2020-12-17 13:15:58 +0100

    Header And footer

---
+++ MainStonks/config.php
@@ -0,0 +1,8 @@
+<?php
+    class config
+    {
+        const USERNAME="PHPStonks";
+        const MOTDEPASSE="stonks";
+        const SERVEUR="phpStonks";
+    }
+?>
---
+++ phpStonks/footer.php
@@ -0,0 +1,27 @@
+<?php
+function mon_footer(){
+
+?>
+
+    <!doctype html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../cssStonks/footer.css" type="text/css">
+        <title>footer</title>
+    </head>
+    <body>
+    </body>
+    <footer>
+        <div class="footer">
+            <span>Axel Baudoin / Thomas Gravy</span>
+            <span>Projet BGC</span>
+        </div>
+    </footer>
+    </html>
+<?php
+}
+?>
