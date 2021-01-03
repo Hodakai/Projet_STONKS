163a31e7009a54001adb43f1698730c8d5ff5ac1
Hodakai <gravy.thom@gmail.com>
2021-01-03 15:14:26 +0100

    Reglage du probleme d'affichage des lots

--- détail-vente.php
+++ détail-vente.php
@@ -8,7 +8,7 @@ $id = filter_input(INPUT_GET, "id");
 require_once "Config.php";
 $pdo = new PDO("mysql:host=" . Config::SERVEUR . ";dbname=phpstonks", Config::UTILISATEUR, Config::MDP);
 
-$requete = $pdo->prepare("select Nom from lot where id_vente = ':id'");
+$requete = $pdo->prepare("select Nom from lot where id_vente = :id");
 
 $requete->bindParam(":id", $id);
 
@@ -30,7 +30,7 @@ MyHeader("Détail de la vente");
         <div class="col-3">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Nom : <?php echo htmlspecialchars($lignes[$i]["nomVente"])?></h5>
+                    <h5 class="card-title">Nom : <?php echo htmlspecialchars($lignes[$i]["Nom"])?></h5>
                     <a href="détail-vente.php?id=<?php echo htmlspecialchars($lignes[$i]["id"]) ?>" class="btn btn-sm btn-primary">Voir les enchères du lot...</a>
                     <a href="modifier-vente.php?id=<?php echo htmlspecialchars($lignes[$i]["id"]) ?>" class="btn btn-sm btn-warning">Modifier</a>
                     <a href="supprimer-vente.php?id=<?php echo htmlspecialchars($lignes[$i]["id"])?>" class="btn btn-sm btn-danger">Supprimer</a>
