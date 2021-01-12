f32b7d48e0893ed6f226f3c671ec3bb402638258
Axebita <Axel.baudoin@epsi.fr>
2021-01-12 11:30:43 +0100

    after branches suppresion

--- MainStonks/ParticiperEnchere.php
+++ MainStonks/ParticiperEnchere.php
@@ -5,11 +5,11 @@ mon_header("participation a une enchère");
 mon_footer("participation a une enchère");
 
 require_once "../MainStonks/config.php";
-$id=filter_input(INPUT_GET,"id");
+$id = filter_input(INPUT_GET, "id");
 
 $pdo = new PDO("mysql:host=" . Config::SERVEUR . ";dbname=phpstonks", Config::USERNAME, Config::MDP);
 $requetes = $pdo->prepare("select * from objet where lot=:id");
-$requetes->bindParam(":id",$id);
+$requetes->bindParam(":id", $id);
 $requetes->execute();
 
 $lignes = $requetes->fetchall();
@@ -33,37 +33,42 @@ $lignes = $requetes->fetchall();
 
     <?php
     for ($i = 0;
-    $i < count($lignes);
-    $i++){
-    ?>
+         $i < count($lignes);
+         $i++) {
+        ?>
 
-    <div class="uneVente">
-        <img src="https://i.kym-cdn.com/entries/icons/original/000/029/959/Screen_Shot_2019-06-05_at_1.26.32_PM.jpg" class="nomVente">
-        <img class="nomVente"><?php echo htmlspecialchars($lignes[$i]["Photo2"]) ?>
-        <img  class="nomVente"><?php echo htmlspecialchars($lignes[$i]["Photo3"]) ?>
-    <p class="nomVente"><?php echo htmlspecialchars($lignes[$i]["NomObjet"]) ?></p>
-    <p class="nomVente"><?php echo htmlspecialchars($lignes[$i]["vendeur"]) ?></p>
-    <p class="nomVente"><?php echo htmlspecialchars($lignes[$i]["prixDepart"]) ?></p>
-    <p class="nomVente"><?php echo htmlspecialchars($lignes[$i]["prixReserve"]) ?></p>
-    <p class="nomVente"><?php echo htmlspecialchars($lignes[$i]["Description"]) ?></p>
+        <div class="uneVente">
+            <img src="https://i.kym-cdn.com/entries/icons/original/000/029/959/Screen_Shot_2019-06-05_at_1.26.32_PM.jpg"
+                 class="nomVente">
+            <img class="nomVente"><?php echo htmlspecialchars($lignes[$i]["Photo2"]) ?>
+            <img class="nomVente"><?php echo htmlspecialchars($lignes[$i]["Photo3"]) ?>
+            <p class="nomVente"><?php echo htmlspecialchars($lignes[$i]["NomObjet"]) ?></p>
+            <p class="nomVente"><?php echo htmlspecialchars($lignes[$i]["vendeur"]) ?></p>
+            <p class="nomVente"><?php echo htmlspecialchars($lignes[$i]["prixDepart"]) ?></p>
+            <p class="nomVente"><?php echo htmlspecialchars($lignes[$i]["prixReserve"]) ?></p>
+            <p class="nomVente"><?php echo htmlspecialchars($lignes[$i]["Description"]) ?></p>
 
-    <div class="ParticipationEnchere">
-        <form action="../actions_client/ActionParticipation.php" method="post">
-            <div class="form-group">
-                <label for="user">Entrez un prix</label>
-                <input  alt="ChoixUser" name="ChoixUser" id="user" type="number" required>
+            <div class="ParticipationEnchere">
+                <form action="../actions_client/ActionParticipation.php" method="post">
+                    <div class="form-group">
+                        <label for="user">Entrez un prix</label>
+                        <input alt="ChoixUser" name="ChoixUser" id="user" type="number" required>
+                    </div>
+                    <br><br>
+                    <input type="submit" class="btn btn-outline-primary">
+                    <br><br>
+
+
+                    <span class="barreIndic">_________</span>
+
+                </form>
             </div>
-            <br><br>
-            <input type="submit" class="btn btn-outline-primary">
+        </div>
 
-        </form>
-    </div>
-</div>
+        <?php
+    }
 
-<?php
-}
-
-?>
+    ?>
 
 </div>
 </html>
